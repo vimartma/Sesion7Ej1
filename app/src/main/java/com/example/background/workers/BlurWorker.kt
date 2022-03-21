d99fde130e88b49e5a97d2497853d130d3d29eb1
Victor <vimartma@unirioja.es>
2022-03-21 11:59:30 +0100

    Initial commitEj1

---
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -0,0 +1,47 @@
+package com.example.background.workers
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
+import com.example.background.R
+
+class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+
+    override fun doWork(): Result {
+        val appContext = applicationContext
+
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+
+        makeStatusNotification("Blurring image", appContext)
+
+        sleep()
+
+        return try {
+            if (TextUtils.isEmpty(resourceUri)) {
+                throw IllegalArgumentException("Invalid input uri")
+            }
+
+            val resolver = appContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri)))
+
+            val output = blurBitmap(picture, appContext)
+
+            // Write bitmap to a temp file
+            val outputUri = writeBitmapToFile(appContext, output)
+
+            val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+            Result.success(outputData)
+        } catch (throwable: Throwable) {
+            Result.failure()
+        }
+    }
+
+}
